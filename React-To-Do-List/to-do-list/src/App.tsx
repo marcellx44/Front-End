a45741714bb43e813acae4ce570ff398880ca38f
Marcella Oliveira <marcella.oliveira.diasss@gmail.com>
2025-12-11 15:46:42 -0300

    Adicionei uma variável de estado em TodoForm para controlar o valor que o usuário digta no input

--- React-To-Do-List/to-do-list/src/App.tsx
+++ React-To-Do-List/to-do-list/src/App.tsx
@@ -3,7 +3,22 @@ import { Header } from "./components/Header/Header.tsx";
 import { Principal} from "./components/Principal/Principal.tsx";
 import { Sidebar } from "./components/Sidebar/Sidebar.tsx";
 import { TodoList } from './components/TodoList/TodoList.tsx';
+import { useState } from "react";
+
 function App() {
+
+//criar o tipo do item
+
+// type Item= {
+//   id: number;
+//   text: string;
+// }
+// //declara o array junto com o useState
+// const [lista, setLista ] = useState<Item[]>([]);
+// function AdicionarItem(){
+//   setLista()  
+// }
+
 
   return (  
         <>
--- React-To-Do-List/to-do-list/src/components/TodoForm/TodoForm.tsx
+++ React-To-Do-List/to-do-list/src/components/TodoForm/TodoForm.tsx
@@ -1,10 +1,19 @@
 import "./to-do-form.estilos.css"
+import { useState, type ChangeEvent } from "react";
+
 export function TodoForm(){
+    const [texto, setTexto]= useState('');
+
+    const MudarTexto= (elemento: ChangeEvent<HTMLInputElement> )=>{
+        setTexto(elemento.target.value)
+        console.log(texto);
+    }
+
     return(
         <form action="" className="container-form">
             <label htmlFor="Tarefa">Digite uma tarefa:</label>
             <div className="container-submit">
-                <input type="text" placeholder="Digite aqui" />
+                <input onChange={MudarTexto} type="text" placeholder="Digite aqui" />
                 <button id="add-tarefa">+</button>
             </div>
         </form>
