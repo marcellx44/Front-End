657af799b1dcdcc72dd967bc5f7633b537b64e74
Marcella Oliveira <marcella.oliveira.diasss@gmail.com>
2025-12-26 14:45:13 -0300

    reducer implementado

--- React-To-Do-List/to-do-list/src/App.tsx
+++ React-To-Do-List/to-do-list/src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from "./components/Header/Header.tsx";
 import { Principal } from "./components/Principal/Principal.tsx";
 import { Sidebar } from "./components/Sidebar/Sidebar.tsx";
 import { TodoList } from './components/TodoList/TodoList.tsx';
-import { useState, createContext } from "react";
+import { createContext, useReducer } from "react";
 
 type TaskType = {
   taskId: string;
@@ -39,58 +39,120 @@ export const ItemContextProvider = createContext(
       }
     ],
     addTask: (text: string, listKey: string) => { },
-    removeTask: (listKey:string, taskId:string) => { },
+    removeTask: (listKey: string, taskId: string) => { },
     addList: (name: string) => { }
   }
 )
 
 function App() {
+  type Action =
+    | { type: 'addTask', payload: { text: string, listKey: string } }
+    | { type: 'removeTask', payload: { listKey: string, taskId: string } }
+    | { type:  'addList', payload: {name: string}}
 
-  const [lists, setLists] = useState<ListType[]>([]); //lista de to do lists
-  const addTask = (text: string, listKey: string) => {
 
-    const copyList = lists.map(
-      list => {
-        if (list.listKey === listKey) {
-          const updatedList = { //cria um novo objeto to do list
-            ...list,
-            tasksList: [...list.tasksList, { taskId: 'item-' + list.tasksList.length.toString(), text: text }] //adiciona o item novo
+  const reducer = (lists: ListType[], action: Action) => {
+    switch (action.type) {
+      case ('addTask'):
+        const newList = lists.map(
+          list => {
+            if (list.listKey === action.payload.listKey) {
+              const updatedList = { //cria um novo objeto to do list
+                ...list,
+                tasksList: [...list.tasksList, { taskId: 'item-' + list.tasksList.length.toString(), text: action.payload.text }] //adiciona o item novo
+              }
+              return updatedList;
+            }
+            return list;
           }
-          return updatedList;
-        }
-        return list;
-      }
-    )
-    setLists(copyList)
-    console.log(lists)
+        )
+        return newList;
+      case ('removeTask'):
+        const copyList = lists.map(
+          list => {
+            if (list.listKey === action.payload.listKey) {
+              const updatedList = {
+                ...list,
+                tasksList: list.tasksList.filter(
+                  task => task.taskId !== action.payload.taskId
+                )
+              }
+              return updatedList;
+            }
+            return list;
+          }
+        )
+        return copyList
+      case ('addList'):
+        return [...lists, ]
+    }
   }
 
-  const removeTask = (listKey:string, taskId: string) => {
-    const copyList = lists.map(
-      list=>{
-        if(list.listKey===listKey){
-          const updatedList = {
-            ...list, 
-            tasksList: list.tasksList.filter(
-              task=> task.taskId !== taskId
-            )
-          }
-          return updatedList;
-        }
-        return list;
-      }
-    )
-    setLists(copyList)
-  }
-
-  const addList = (name: string) => { //funcao de adicionar listas
-    setLists([...lists,
+  const initialState = [
     {
-      listKey: 'list-' + lists.length.toString(),
-      name: name,
-      tasksList: []
-    }]);
+      listKey: '0',
+      name: '',
+      tasksList: [
+        {
+          taskId: '',
+          text: ''
+        }
+      ]
+    }
+  ]
+
+  const [lists, dispatch] = useReducer(reducer, initialState); //lista de to do lists
+  // const addTask = (text: string, listKey: string) => {
+
+  //   const newList = lists.map(
+  //     list => {
+  //       if (list.listKey === listKey) {
+  //         const updatedList = { //cria um novo objeto to do list
+  //           ...list,
+  //           tasksList: [...list.tasksList, { taskId: 'item-' + list.tasksList.length.toString(), text: text }] //adiciona o item novo
+  //         }
+  //         return updatedList;
+  //       }
+  //       return list;
+  //     }
+  //   )
+  //   return (newList);
+  // }
+
+  // const removeTask = (listKey: string, taskId: string) => {
+  //   const copyList = lists.map(
+  //     list => {
+  //       if (list.listKey === listKey) {
+  //         const updatedList = {
+  //           ...list,
+  //           tasksList: list.tasksList.filter(
+  //             task => task.taskId !== taskId
+  //           )
+  //         }
+  //         return updatedList;
+  //       }
+  //       return list;
+  //     }
+  //   )
+  //   setLists(copyList)
+  // }
+
+  const addTask = (text: string, listKey: string) => {
+    dispatch({ type: 'addTask', payload: { text, listKey } })
   }
+
+  const removeTask = (listKey: string, taskId: string) => {
+    dispatch({ type: 'removeTask', payload: { listKey, taskId } })
+  }
+
+  // const addList = (name: string) => { //funcao de adicionar listas
+  //   setLists([...lists,
+  //   {
+  //     listKey: 'list-' + lists.length.toString(),
+  //     name: name,
+  //     tasksList: []
+  //   }]);
+  // }
 
   return (
     <ItemContextProvider.Provider value={{ lists, addTask, removeTask, addList }}>
@@ -108,7 +170,7 @@ function App() {
                 <TodoList key={list.listKey} name={list.name} listKey={list.listKey
 
                 }>
-                  
+
                 </TodoList>
               )
             )
