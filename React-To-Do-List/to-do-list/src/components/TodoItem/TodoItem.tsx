e91564ad652fb2dd20f6b902d5c142b0b0730e91
Marcella Oliveira <marcella.oliveira@quantumfinance.com.br>
2026-01-05 15:44:54 -0300

    modificações no estilo

--- React-To-Do-List/to-do-list/src/components/TodoItem/TodoItem.tsx
+++ React-To-Do-List/to-do-list/src/components/TodoItem/TodoItem.tsx
@@ -1,19 +1,44 @@
+import { createTheme, Dialog, DialogContentText } from "@mui/material";
 import { ItemContextProvider } from "../../App";
 import "./to-do-item.estilos.css"
-import { useContext } from "react";
+import { useContext, useState } from "react";
+
+const DialogTheme = createTheme({
+    palette: {
+        primary:{
+            main: '#1976d2'
+        }
+    }
+})
 
 
-export const TodoItem=({taskId, text, listKey}:{taskId:string, text: string, listKey:string})=>{
-    const {removeTask} = useContext(ItemContextProvider)
+export const TodoItem = ({ taskId, text, listKey }: { taskId: string, text: string, listKey: string }) => {
+    const [open, setOpen] = useState(false)
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    }
+    const { removeTask } = useContext(ItemContextProvider)
 
-    const handleRemove= ()=>{
+    const handleRemove = () => {
         removeTask(listKey, taskId)
     }
-    return(
-        <li className="container-item" key={taskId}>
-            <input type="checkbox" id='check' />
-            <p>{text}</p>
-            <button id="excluir" onClick={handleRemove}>X</button>
-        </li>
+    return (
+     
+            <li className="container-item" key={taskId}>
+                <input type="checkbox" id='check' />
+                <p>{text}</p>
+                <a href="#" onClick={handleClickOpen}>Exibir detalhes</a>
+                <Dialog
+                    open= {open}
+                    onClose= {handleClose}
+                >
+                    <DialogContentText>{text}</DialogContentText>
+                </Dialog>
+                <button id="excluir" onClick={handleRemove}>X</button>
+            </li >
+      
     )
 }
